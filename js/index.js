0e66ac7f89c2f342446b2f3b0d42b65f1075964f
AiGaoShiBOY <952445590@qq.com>
2021-06-23 22:14:53 +0800

    fix

--- js/index.js
+++ js/index.js
@@ -618,9 +618,7 @@ function ShowElement(element) {
     var newobj = document.createElement('input');
     newobj.type = 'text';
     newobj.value = oldhtml;
-    //为新增元素添加光标离开事件
     newobj.onblur = function() {
-        //当触发时设置父节点的双击事件为ShowElement、也可在这里进行保存动作
         element.setAttribute("ondblclick", "ShowElement(this);");
         if(this.value==''){
             element.innerHTML = oldhtml;
@@ -639,15 +637,12 @@ function ShowElement(element) {
         saveData(data);
         renderAllTodo();
     }
-    //设置该标签的子节点为空
     element.innerHTML = '';
-    //添加该标签的子节点，input对象
     element.appendChild(newobj);
-    //设置选择文本的内容或设置光标位置（两个参数：start,end；start为开始位置，end为结束位置；如果开始位置和结束位置相同则就是光标位置）
     newobj.setSelectionRange(0, oldhtml.length);
     //设置获得光标
     newobj.focus();
-    //设置父节点的双击事件为空
+    //不能重复点击
     newobj.parentNode.setAttribute("ondblclick", "");
 }
 
@@ -661,7 +656,7 @@ function showDate(element) {
     newobj.type = 'datetime-local';
     newobj.value = year+'T'+time;
     newobj.required='required';
-    //为新增元素添加光标离开事件
+
     newobj.onblur = function() {
         element.setAttribute("ondblclick", "showDate(this);");
         i = element.parentNode.id.substr(1);
@@ -685,14 +680,12 @@ function showDate(element) {
         saveData(data);
         renderAllTodo();
     }
-    //设置该标签的子节点为空
+
     element.innerHTML = '';
-    //添加该标签的子节点，input对象
+
     element.appendChild(newobj);
-    //设置选择文本的内容或设置光标位置（两个参数：start,end；start为开始位置，end为结束位置；如果开始位置和结束位置相同则就是光标位置）
-    //设置获得光标
+  
     newobj.focus();
-    //设置父节点的双击事件为空
     newobj.parentNode.setAttribute("ondblclick", "");
 }
 
